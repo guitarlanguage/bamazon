34ccf703f94f97a73521182b8819b0c81ef4b7f5
Tom LaChance <guitarlanguageseries@gmail.com>
2018-03-01 16:59:43 -0600

    added require inquirer and began prompting question

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -16,17 +17,55 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+
+    whichProductWouldYouLike();
     itemsForSale();
+    connection.end();
 });
 
 function itemsForSale() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
+        console.log(`
+            `);
         res.forEach(function(elem) {
-            console.log(`Product ${elem.item_id}: ${elem.product_name} | Department: ${elem.department_name} | Price: $${elem.price} | In Stock: ${elem.stock_quantity}`);
+            console.log(`Id Number: ${elem.item_id}: ${elem.product_name} | Department: ${elem.department_name} | Price: $${elem.price} | In Stock: ${elem.stock_quantity}`);
+            console.log(`--------------------------------------------------------------------------------------`);
         });
         // console.log(res);
-        connection.end();
+
     });
 }
+
+
+var whichProductWouldYouLike = function () {
+  inquirer
+    .prompt({
+      name: "whichId",
+      type: "input",
+      message: "Please provide the id number of the product that you would like to buy?"
+    })
+    .then(function(answer) {
+
+        console.log(answer.whichId);
+    //   switch (answer.action) {
+    //     case "1":
+    //       artistSearch();
+    //       break;
+    //
+    //     case "Find all artists who appear more than once":
+    //       multiSearch();
+    //       break;
+    //
+    //     case "Find data within a specific range":
+    //       rangeSearch();
+    //       break;
+    //
+    //     case "Search for a specific song":
+    //       songSearch();
+    //       break;
+    //   }
+    // });
+});
+};

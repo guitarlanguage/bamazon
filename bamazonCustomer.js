f074ee4a5a138a43265880fdff2d7a57321fc754
Tom LaChance <guitarlanguageseries@gmail.com>
2018-03-01 17:27:44 -0600

    bamazonCustomer.js now takes in the users input and directs it

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,55 +17,77 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-
-    whichProductWouldYouLike();
     itemsForSale();
+    whichProductWouldYouLike();
+
     connection.end();
 });
 
 function itemsForSale() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        // Log all results of the SELECT statement
+        // Log space for better terminal viewing experience
         console.log(`
             `);
+        //log each element in the products table
         res.forEach(function(elem) {
-            console.log(`Id Number: ${elem.item_id}: ${elem.product_name} | Department: ${elem.department_name} | Price: $${elem.price} | In Stock: ${elem.stock_quantity}`);
+            console.log(`Id Number: ${elem.item_id} | ${elem.product_name} | Department: ${elem.department_name} | Price: $${elem.price} | In Stock: ${elem.stock_quantity}`);
             console.log(`--------------------------------------------------------------------------------------`);
         });
-        // console.log(res);
-
     });
 }
 
-
-var whichProductWouldYouLike = function () {
-  inquirer
+//ask the user which product they would like to purchase
+var whichProductWouldYouLike = function() {
+inquirer
     .prompt({
-      name: "whichId",
-      type: "input",
-      message: "Please provide the id number of the product that you would like to buy?"
+        name: "whichId",
+        type: "input",
+        message: "Which product would you like to buy? Please provide the id number."
     })
     .then(function(answer) {
 
-        console.log(answer.whichId);
-    //   switch (answer.action) {
-    //     case "1":
-    //       artistSearch();
-    //       break;
-    //
-    //     case "Find all artists who appear more than once":
-    //       multiSearch();
-    //       break;
-    //
-    //     case "Find data within a specific range":
-    //       rangeSearch();
-    //       break;
-    //
-    //     case "Search for a specific song":
-    //       songSearch();
-    //       break;
-    //   }
-    // });
-});
+        // console.log(answer.whichId);
+        switch (answer.whichId) {
+            case '1':
+                console.log(`great choice`);
+                break;
+
+            case '2':
+                console.log(`great choice 2`);
+                break;
+
+            case '3':
+                console.log(`great choice 2`);
+                break;
+
+            case '4':
+                console.log(`great choice 4`);
+                break;
+
+            case '5':
+                console.log(`great choice`);
+                break;
+
+            case '6':
+                console.log(`great choice 2`);
+                break;
+
+            case '7':
+                console.log(`great choice 2`);
+                break;
+
+            case '8':
+                console.log(`great choice 4`);
+                break;
+
+            case '9':
+                console.log(`great choice 2`);
+                break;
+
+            case '10':
+                console.log(`great choice 4`);
+                break;
+        }
+    });
 };

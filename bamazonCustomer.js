4e21e889685bb13792071ee4e5b3ffcea5e4c6a4
Tom LaChance <guitarlanguageseries@gmail.com>
2018-03-02 11:36:39 -0600

    dropped switch statement, added second question to prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,9 +18,7 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     itemsForSale();
-    whichProductWouldYouLike();
-
-    connection.end();
+    // connection.end();
 });
 
 function itemsForSale() {
@@ -44,50 +42,26 @@ inquirer
         name: "whichId",
         type: "input",
         message: "Which product would you like to buy? Please provide the id number."
+    }, {
+        name: "howMany",
+        type: "input",
+        message: "How many do you need?",
+        /* Legacy way: with this.async */
     })
     .then(function(answer) {
-
-        // console.log(answer.whichId);
-        switch (answer.whichId) {
-            case '1':
-                console.log(`great choice`);
-                break;
-
-            case '2':
-                console.log(`great choice 2`);
-                break;
-
-            case '3':
-                console.log(`great choice 2`);
-                break;
-
-            case '4':
-                console.log(`great choice 4`);
-                break;
-
-            case '5':
-                console.log(`great choice`);
-                break;
-
-            case '6':
-                console.log(`great choice 2`);
-                break;
-
-            case '7':
-                console.log(`great choice 2`);
-                break;
-
-            case '8':
-                console.log(`great choice 4`);
-                break;
-
-            case '9':
-                console.log(`great choice 2`);
-                break;
-
-            case '10':
-                console.log(`great choice 4`);
-                break;
-        }
+        //
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            // Log space for better terminal viewing experience
+            console.log(`
+                `);
+            //log each element in the products table
+            res.forEach(function(elem) {
+                console.log(`Id Number: ${elem.item_id} | ${elem.product_name} | Department: ${elem.department_name} | Price: $${elem.price} | In Stock: ${elem.stock_quantity}`);
+                console.log(`--------------------------------------------------------------------------------------`);
+            });
+        });
     });
 };
+
+whichProductWouldYouLike();
